0348564580c1664498192f2302b74d09b187fcf4
youngjae.jeon <yjeon98@hanyang.ac.kr>
2025-05-23 16:40:06 +0900

    modify server file

--- fastapi-model-serving/LaneDetection/server.py
+++ fastapi-model-serving/LaneDetection/server.py
@@ -4,6 +4,8 @@ from image_lane_detection import get_lane_detector, get_lane_detections
 from starlette.responses import Response, JSONResponse
 import torch
 from fastapi import FastAPI, File
+import base64
+import numpy as np
 
 device = "cuda:2" if torch.cuda.is_available() else "cpu"
 # cls_model = get_classifier(device)
@@ -18,14 +20,40 @@ app = FastAPI(
     version="0.1.0",
 )
 
+# @app.post("/lane_detection")
+# def get_lane_detection(file: bytes = File(...)):
+#     """Get lane detection from image file"""
+#     lane_image, lane_detection = get_lane_detections(lane_detection_model, file, device)
+#     bytes_io = io.BytesIO()
+#     lane_image.save(bytes_io, format="PNG")
+
+#     return Response(bytes_io.getvalue(), media_type="image/png")
+def convert_to_serializable(obj):
+    """
+    obj가 numpy.ndarray이면 리스트로 변환하고,
+    리스트나 dict 내에 있는 ndarray도 재귀적으로 처리합니다.
+    """
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    elif isinstance(obj, list):
+        return [convert_to_serializable(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {k: convert_to_serializable(v) for k, v in obj.items()}
+    else:
+        return obj
+
 @app.post("/lane_detection")
 def get_lane_detection(file: bytes = File(...)):
     """Get lane detection from image file"""
     lane_image, lane_detection = get_lane_detections(lane_detection_model, file, device)
     bytes_io = io.BytesIO()
     lane_image.save(bytes_io, format="PNG")
+    image_bytes = bytes_io.getvalue()
+    image_b64 = base64.b64encode(image_bytes).decode("utf-8")
+    
+    lane_detection = convert_to_serializable(lane_detection)
 
-    return Response(bytes_io.getvalue(), media_type="image/png")
+    return JSONResponse(content={"image": image_b64, "detection_result": lane_detection})
 
 # @app.post("/test")
 # def get_classification_map(file: bytes = File(...)):
--- fastapi-model-serving/streamlit/ui.py
+++ fastapi-model-serving/streamlit/ui.py
@@ -7,6 +7,7 @@ import streamlit as st
 import json
 import base64
 import numpy as np
+from fastapi import UploadFile
 
 # interact with FastAPI endpoint
 # segmentation_backend = "http://fastapi:8000/segmentation"
@@ -25,10 +26,21 @@ def dict_to_numpy(json_str):
 segmentation_backend = "http://localhost:8000/segmentation"
 detection_yolov10_backend = "http://localhost:8000/detection_yolov10"
 weather_classification_backend = "http://localhost:8000/weather_classification"
+time_classification_backend = "http://localhost:8000/time_classification"
+image_backend = "http://localhost:8000/image"
 lane_detection_backend = "http://localhost:8001/lane_detection"
 
 def process(image, server_url: str):
     m = MultipartEncoder(fields={"file": ("filename", image, "image/jpeg")})
+    r = requests.post(
+        server_url, data=m, headers={"Content-Type": m.content_type}, timeout=8000
+    )
+    return r
+
+def process_image(uploade_file, server_url: str):
+    file_bytes = uploade_file.getvalue()
+    file_name = uploade_file.name
+    m = MultipartEncoder(fields={"file": (file_name, file_bytes, "image/jpeg")})
     r = requests.post(
         server_url, data=m, headers={"Content-Type": m.content_type}, timeout=8000
     )
@@ -45,11 +57,144 @@ st.write(
 
 input_image = st.file_uploader("Insert image")  # image upload widget
 
+if input_image:
+    image_process = process_image(input_image, image_backend)
+    image_result = image_process.content
+    weather_process = process(input_image, weather_classification_backend)
+    weather_result = weather_process.content
+    time_process = process(input_image, time_classification_backend)
+    time_result = time_process.content
+    detection_process = process(input_image, detection_yolov10_backend)
+    detection_result = detection_process.content
+    lane_detection_process = process(input_image, lane_detection_backend)
+    image_result = image_result.decode("utf-8")
+
+    if isinstance(weather_result, bytes):
+        weather_result = weather_result.decode("utf-8")
+    else:
+        str(weather_result)
+    if isinstance(time_result, bytes):
+        time_result = time_result.decode("utf-8")
+    else:
+        str(time_result)
+    if isinstance(detection_result, bytes):
+        detection_result = detection_result.decode("utf-8")
+    else:
+        str(detection_result)
+    
+    image_data = json.loads(image_result)
+    image_info = image_data["image_info"]
+    weather_data = json.loads(weather_result)
+    weather_class = weather_data["weather_class"]
+    time_data = json.loads(time_result)
+    time_class = time_data["time_class"]
+    detection_data = json.loads(detection_result)
+    detection_list = detection_data["detection_result"]
+    
+    lane_detection_data = lane_detection_process.json()
+    image_b64 = lane_detection_data.get("image")
+    lane_detection_result = lane_detection_data.get("detection_result")
+    image_bytes = base64.b64decode(image_b64)
+    lane_detection_image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    structured_result = {
+        "Image_information": {
+            "file_name": image_info["name"],
+            "width": image_info["width"],
+            "height": image_info["height"],
+            "format": image_info["format"],
+            "mode": image_info["mode"]
+        },
+        "Time_information": {
+            "class": time_class
+        },
+        "Weather_information": {
+            "class": weather_class
+        },
+        "Detection_information": {
+            "num_of_bbox": len(detection_list),
+            "bbox_info": []
+        },
+        "Lane_Detection_information": {
+            "num_of_lanes": len(lane_detection_result),
+            "lane_info": []
+        }
+    }
+    
+    for box in detection_list:
+        class_label = box[0]
+        x1, y1, x2, y2 = box[1], box[2], box[3], box[4]
+        
+        structured_result["Detection_information"]["bbox_info"].append({
+        "class": class_label,
+        "type": "Bounding_box",
+        "bbox_x1": x1,
+        "bbox_y1": y1,
+        "bbox_x2": x2,
+        "bbox_y2": y2
+    })
+    for lane in lane_detection_result:
+        structured_result["Lane_Detection_information"]["lane_info"].append({
+        "type": "Line",
+        "points": lane
+    })
+        
+    # JSON 문자열로 변환합니다.
+    json_data = json.dumps(structured_result, indent=4)
+    st.download_button(
+        label="Download JSON Results",
+        data=json_data,
+        file_name="results.json",
+        mime="application/json"
+    )
+    
+# if st.button("download JSON results"):
+#     if input_image:
+#         weather_process = process(input_image, weather_classification_backend)
+#         weather_result = weather_process.content
+#         detection_process = process(input_image, detection_yolov10_backend)
+#         detection_result = detection_process.content
+#         lane_detection_process = process(input_image, lane_detection_backend)
+#         lane_detection_image = Image.open(io.BytesIO(lane_detection_process.content)).convert("RGB")
+#         # lane_detection_process = process(input_image, lane_detection_backend)
+#         # print("Response text:", lane_detection_process.text)
+#         # lane_detection_data = lane_detection_process.json()
+#         # lane_image = base64.b64decode(lane_detection_data["image"])
+#         # lane_detection_image = Image.open(io.BytesIO(lane_image)).convert("RGB")
+#         # lane = lane_detection_data["detection_result"]
+        
+#         results = {
+#             "weather": weather_result.decode("utf-8") if isinstance(weather_result, bytes) else str(weather_result),
+#             "detection": detection_result.decode("utf-8") if isinstance(detection_result, bytes) else str(detection_result),
+#             # lane_detection 결과가 이미지이므로, base64 인코딩으로 저장할 수 있습니다.
+#             # "lane_detection": base64.b64encode(lane_detection_process.content).decode("utf-8")
+#         }
+#         # JSON 문자열로 변환합니다.
+#         json_data = json.dumps(results, indent=4)
+        
+#         # JavaScript를 이용해 자동 다운로드 실행 (파일 저장 위치 선택 창이 뜹니다)
+#         download_js = f"""
+#         <script>
+#         var a = document.createElement('a');
+#         a.href = 'data:application/json;charset=utf-8,' + encodeURIComponent(`{json_data}`);
+#         a.download = 'results.json';
+#         document.body.appendChild(a);
+#         a.click();
+#         document.body.removeChild(a);
+#         </script>
+#         """
+#         st.markdown(download_js, unsafe_allow_html=True)
+#         st.success("JSON 결과가 다운로드됩니다.")
+#     else:
+#         st.error("Insert an image!")
+
 if st.button("get total result"):
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
     if input_image:
         weather_process = process(input_image, weather_classification_backend)
         weather_result = weather_process.content
+        time_process = process(input_image, time_classification_backend)
+        time_result = time_process.content
         detection_process = process(input_image, detection_yolov10_backend)
         detection_result = detection_process.content
         lane_detection_process = process(input_image, lane_detection_backend)
@@ -57,10 +202,12 @@ if st.button("get total result"):
         
         col1.header("Lane detection result")
         col1.image(lane_detection_image, use_column_width=True)
-        col2.header("Weather")
-        col2.write(weather_result)
-        col3.header("Detection")
-        col3.write(detection_result)
+        col2.header("Time")
+        col2.write(time_result)
+        col3.header("Weather")
+        col3.write(weather_result)
+        col4.header("Detection")
+        col4.write(detection_result)
 
 if st.button("get lane detection result"):
     col1, col2 = st.columns(2)
@@ -87,18 +234,33 @@ if st.button("get lane detection result"):
 #     else:
 #         st.write("Insert an image!")
 
-
-if st.button("Get weather classification"):
+if st.button("Get time classification"):
     col1, col2 = st.columns(2)
 
     if input_image:
-        weather_result = process(input_image, weather_classification_backend)
+        time_process = process(input_image, time_classification_backend)
+        time_result = time_process.content
         original_image = Image.open(input_image).convert("RGB")
         # classified_image = Image.open(io.BytesIO(classifications.content)).convert("RGB")
         col1.header("Original")
         col1.image(original_image, use_column_width=True)
         col2.header("Classified")
-        col2.write(weather_result.content)
+        col2.write(time_result)
+    else:
+        st.write("Insert an image!")
+
+if st.button("Get weather classification"):
+    col1, col2 = st.columns(2)
+
+    if input_image:
+        weather_process = process(input_image, weather_classification_backend)
+        weather_result = weather_process.content
+        original_image = Image.open(input_image).convert("RGB")
+        # classified_image = Image.open(io.BytesIO(classifications.content)).convert("RGB")
+        col1.header("Original")
+        col1.image(original_image, use_column_width=True)
+        col2.header("Classified")
+        col2.write(weather_result)
     else:
         st.write("Insert an image!")
         
@@ -107,13 +269,14 @@ if st.button("Get detection yolo map"):
 
     if input_image:
         # JSONResponse(content={"detection_result": detection_result})
-        detections = process(input_image, detection_yolov10_backend)
+        detection_process = process(input_image, detection_yolov10_backend)
+        detection_result = detection_process.content
         original_image = Image.open(input_image).convert("RGB")
         # detected_image = Image.open(io.BytesIO(detections.content)).convert("RGB")
         col1.header("Original")
         col1.image(original_image, use_column_width=True)
         col2.header("Detected")
         # col2.image(detected_image, use_column_width=True)
-        col2.write(detections.content)
+        col2.write(detection_result)
     else:
         st.write("Insert an image!")        
